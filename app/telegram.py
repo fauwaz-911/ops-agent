1f1b8567578cfdce287418da793e014a27abdbed
fauwaz-911 <60237483+fauwaz-911@users.noreply.github.com>
2026-02-21 17:26:53 +0100

    Update telegram.py

--- app/telegram.py
+++ app/telegram.py
@@ -4,7 +4,7 @@ import requests
 TELEGRAM_TOKEN = os.getenv("TELEGRAM_TOKEN")
 CHAT_ID = os.getenv("CHAT_ID")
 
-def send_telegram_message(text):
+def send_message(text):
     url = f"https://api.telegram.org/bot{TELEGRAM_TOKEN}/sendMessage"
     payload = {"chat_id": CHAT_ID, "text": text}
     requests.post(url, json=payload)
